5d5c6df86e2620a9dd2fa458e67bdcbadbcaafe5
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-04-26 18:38:44 +0600

    added customer info page

--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -1,17 +1,41 @@
+"use client";
+
 import { DataTable } from "@/components/custom-ui/DataTable";
+import Loader from "@/components/custom-ui/Loader";
 import { orderColumns } from "@/components/orders/OrderColumns";
 import { Separator } from "@/components/ui/separator";
+import { useEffect, useState } from "react";
 
-const OrdersPage = async () => {
-  const res = await fetch("http://localhost:3000/api/orders");
-  const order = await res.json();
-  return (
+const Orders = () => {
+  const [loading, setLoading] = useState(true);
+  const [orders, setOrders] = useState([]);
+
+  const getOrders = async () => {
+    try {
+      const res = await fetch(`/api/orders`);
+      const data = await res.json();
+      setOrders(data);
+      setLoading(false);
+    } catch (err) {
+      console.log("[orders_GET", err);
+    }
+  };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
     <div className="px-10 py-5">
       <p className="text-heading2-bold">Orders</p>
       <Separator className="bg-grey-1 my-5" />
-      <DataTable searchKey="_id" columns={orderColumns} data={order} />
+      <DataTable columns={orderColumns} data={orders} searchKey="_id" />
     </div>
   );
 };
 
-export default OrdersPage;
+export const dynamic = "force-dynamic";
+
+export default Orders;
