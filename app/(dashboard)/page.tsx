bed5535a9c8cd5a0eb0abe5db65d6b47f4e59995
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-04-27 08:23:21 +0600

    added Dashboard statistics

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,52 @@
-import { UserButton } from "@clerk/nextjs";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import {
+  getTotalCustomer,
+  getTotalSales,
+  salesPerMonth,
+} from "@/lib/actions/actions";
+import { CircleDollarSign, ShoppingBag, UserRound } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const { totalOrders, totalRevenue } = await getTotalSales();
+  const totalCustomer = await getTotalCustomer();
+  const graphData = await salesPerMonth();
+
   return (
-    <main>
-      <UserButton />
+    <main className="px-8 py-10 ">
+      <p className="text-heading2-bold">Dashboard</p>
+      <Separator className="bg-grey-1 my-5" />
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center">
+            <CardTitle>Total Revenue</CardTitle>
+            <CircleDollarSign className="max-sm:hidden" />
+          </CardHeader>
+          <CardContent>
+            <p className="text-body-bold">$ {totalRevenue}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center">
+            <CardTitle>Total Orders</CardTitle>
+            <ShoppingBag className="max-sm:hidden" />
+          </CardHeader>
+          <CardContent>
+            <p className="text-body-bold">{totalOrders}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader className="flex flex-row justify-between items-center">
+            <CardTitle>Total Customers</CardTitle>
+            <UserRound className="max-sm:hidden" />
+          </CardHeader>
+          <CardContent>
+            <p className="text-body-bold">{totalCustomer}</p>
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 }
---
+++ lib/actions/actions.ts
@@ -0,0 +1,40 @@
+import Customer from "../models/Customer";
+import Order from "../models/Order";
+import { connectToDB } from "../mongoDB";
+
+export const getTotalSales = async () => {
+  await connectToDB();
+  const orders = await Order.find();
+  const totalOrders = orders.length;
+  const totalRevenue = orders.reduce(
+    (acc, order) => acc + order.totalAmount,
+    0
+  );
+  return { totalOrders, totalRevenue };
+};
+
+export const getTotalCustomer = async () => {
+  await connectToDB();
+  const customers = await Customer.find();
+  const totalCustomer = customers.length;
+  return totalCustomer;
+};
+
+export const salesPerMonth = async () => {
+  await connectToDB();
+  const orders = await Order.find();
+
+  const salesPerMonth = orders.reduce((acc, order) => {
+    const monthIndex = new Date(order.createdAt).getMonth();
+    acc[monthIndex] = (acc[monthIndex] || 0) + order.totalAmount;
+    return acc;
+  }, {});
+
+  const graphData = Array.from({ length: 12 }, (_, i) => {
+    const month = new Intl.DateTimeFormat("en-US", { month: "short" }).format(
+      new Date(new Date(0, i))
+    );
+    return { name: month, sales: salesPerMonth[i] || 0 };
+  });
+  return graphData;
+};
